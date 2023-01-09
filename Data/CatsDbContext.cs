99d47756676e86f4be505225e911a9a0f7d1cb8e
PandorAura <aurapandor@gmail.com>
2023-01-09 13:05:14 +0200

    Add project files.

---
+++ Data/CatsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HappyPaws_BE.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HappyPaws_BE.Data
+{
+    public class CatsDbContext: DbContext
+    {
+        public CatsDbContext(DbContextOptions<CatsDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Cats> Cats { get; set; }
+    }
+}
---
+++ Models/Domain/Cats.cs
@@ -0,0 +1,11 @@
+﻿namespace HappyPaws_BE.Models.Domain
+{
+    public class Cats
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public byte[]  Image { get; set; }
+        public bool IsAdopted { get; set; }
+    }
+}
